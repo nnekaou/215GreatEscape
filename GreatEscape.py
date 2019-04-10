1765daa038f8c325fc3c7b7d8f63ebcefbc3ec84
nnekaou <nnekaou@gmail.com>
2019-04-10 16:39:17 -0400

    just random stuff that may or may not work tbh

--- GreatEscape.py
+++ GreatEscape.py
@@ -9,3 +9,43 @@ The goal of this project is to create an escape room for individuals to go throu
 @author: jl
 """
 
+class player:
+    def __init__(self, name, hints, position):
+        self.name = name
+        self.hints = hints
+        self.position = position
+    
+    
+
+player.position = currentroom
+
+player.hints = knowledge
+
+def chair:
+    print("You approach the chair...")
+    location = print(input("Would you like to check the top, side, or bottom...?"))
+    if location == "top":
+        #TODO
+    elif location == "side":
+        #TODO
+    elif location == "bottom":
+        #TODO
+
+def table:
+    print("")
+
+def board:
+    print("")
+
+def laptop:
+    print("")
+
+def cabinet:
+    print("")
+
+def safe:
+    print("")
+
+def pencil:
+    print("")
+
